960cae63792c12453c7efda1cf20ad43d1495aed
ddmso-iscteiul <ddmso@iscte-iul.pt>
2019-04-15 18:01:44 +0100

    created maven project for java implementation

---
+++ 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/App.java
@@ -0,0 +1,13 @@
+package Sid_Grupo13.Monitorizador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
