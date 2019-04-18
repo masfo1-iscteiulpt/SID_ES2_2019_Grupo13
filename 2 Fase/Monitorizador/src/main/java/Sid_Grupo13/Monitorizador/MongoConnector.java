6adcb1342c505e7520c81e81caceeed14a5df482
ddmso-iscteiul <ddmso@iscte-iul.pt>
2019-04-19 00:10:08 +0100

    Implemented mongoconnector constructor

--- 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MongoConnector.java
+++ 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MongoConnector.java
@@ -1,5 +1,27 @@
 package Sid_Grupo13.Monitorizador;
 
-public class MongoConnector {
+import org.bson.Document;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
+public class MongoConnector {
+	MongoClient mongoClient;
+	
+	public static void main(String[] args) {
+		new MongoConnector();
+	}
+	MongoConnector(){
+		mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase database = mongoClient.getDatabase("teste");
+		MongoCollection<Document> mc=database.getCollection("teste");
+		FindIterable<Document> found=mc.find();
+		for(Document d:found) {
+			System.out.println(d.toJson());
+		}
+		
+	}
 }
