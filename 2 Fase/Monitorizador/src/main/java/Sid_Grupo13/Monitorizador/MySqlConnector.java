92a5649db6542ebba04badfbf707d301ab291f7d
reavh-iscteiul <reavh@iscte-iul.pt>
2019-04-21 00:56:06 +0100

    Added maxId method

--- 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MySqlConnector.java
+++ 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MySqlConnector.java
@@ -1,8 +1,6 @@
 package Sid_Grupo13.Monitorizador;
 
-import java.sql.Array;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,39 +8,50 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class MySqlConnector {
-	
-	Connection conn;
-	public static void main(String[] args) {
-		new MySqlConnector("jdbc:mariadb://localhost:3306/maindb","root","").insert("medicoes_luminusidade",new Timestamp(System.currentTimeMillis()),2.0);
-	}
-	public MySqlConnector(String uri,String user,String password) {
+
+	private Connection connection;
+
+	public MySqlConnector(String uri, String user, String password) {
 		try {
-			conn = DriverManager.getConnection(uri+"?user="+user+"&password="+password);
+			connection = DriverManager.getConnection(uri + "?user=" + user + "&password=" + password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void executeQuery() {
-		PreparedStatement stmt;
 		try {
-			stmt = conn.prepareStatement("Select max(idcultura) from cultura;");
-			ResultSet set=stmt.executeQuery();
+			PreparedStatement statement = connection.prepareStatement("Select max(idcultura) from cultura;");
+			ResultSet set = statement.executeQuery();
 			System.out.println(set.toString());
 			set.next();
 			System.out.println(set.getInt(1));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	public int maxId(String table) {
+		try {
+			PreparedStatement statement = connection.prepareStatement("Select max(id) from " + table + ";");
+			ResultSet resultSet = statement.executeQuery();
+			resultSet.next();
+			return resultSet.getInt(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
 			
+			return 0;
+		}
 	}
 	
-	public void insert(String table,Timestamp date,double value) {
+	public void insert(String table, Timestamp timestamp, double value) {
 		try {
-			PreparedStatement stmt = conn.prepareStatement("insert into "+table+" values(0,?,?);");
-			stmt.setTimestamp(1, date);
-			stmt.setDouble(2, value);
-			stmt.execute();
+			PreparedStatement statement = connection.prepareStatement("insert into " + table + " values(?,?,?);");
+			statement.setInt(1, maxId(table) + 1);
+			statement.setTimestamp(2, timestamp);
+			statement.setDouble(3, value);
+			statement.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
