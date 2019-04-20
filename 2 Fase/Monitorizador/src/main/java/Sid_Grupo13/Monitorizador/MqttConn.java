8f4999d24317649c25dd6cc75efc975d40be6e2d
reavh-iscteiul <reavh@iscte-iul.pt>
2019-04-21 00:52:50 +0100

    Formated code

--- 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MqttConn.java
+++ 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/MqttConn.java
@@ -7,24 +7,28 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MqttConn {
+
 	public static void main(String[] args) {
-		MongoConnector mconn=new MongoConnector("teste");
-		MqttCallback callback=new MqttCallback() {
+		MongoConnector mconn = new MongoConnector("teste", "teste22");
+		MqttCallback callback = new MqttCallback() {
 			@Override
 			public void messageArrived(String topic, MqttMessage message) throws Exception {
 				mconn.insertJson(message.toString());
 			}
+
 			@Override
 			public void deliveryComplete(IMqttDeliveryToken token) {
 			}
+
 			@Override
 			public void connectionLost(Throwable cause) {
 				System.exit(0);
 			}
 		};
+
 		new Poller("iscte_sid_2016_S1", "tcp://iot.eclipse.org:1883", "cliente", callback);
 		System.out.println("Pressione uma tecla para sair...");
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		sc.nextLine();
 		System.out.println("adeus");
 		sc.close();
--- 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/Poller.java
+++ 2 Fase/Monitorizador/src/main/java/Sid_Grupo13/Monitorizador/Poller.java
@@ -6,24 +6,23 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-public class Poller{
+public class Poller {
 
-	String topic; 
+	String topic;
 	String broker;
 	String clientId;
 	MemoryPersistence persistence = new MemoryPersistence();
 	MqttClient sampleClient;
 	MqttConnectOptions connOpts;
-	
-	
-	public Poller(String topic,String broker,String clientId,MqttCallback callback) {
+
+	public Poller(String topic, String broker, String clientId, MqttCallback callback) {
 		try {
-			this.topic=topic;
-			this.broker=broker;
-			this.clientId=clientId;
-			sampleClient=new MqttClient(broker, clientId, persistence);
+			this.topic = topic;
+			this.broker = broker;
+			this.clientId = clientId;
+			sampleClient = new MqttClient(broker, clientId, persistence);
 			sampleClient.setCallback(callback);
-			connOpts= new MqttConnectOptions();
+			connOpts = new MqttConnectOptions();
 			connOpts.setCleanSession(true);
 		} catch (MqttException me) {
 			exeptionMessage(me);
@@ -33,34 +32,33 @@ public class Poller{
 	public void connect() {
 		try {
 			sampleClient.connect(connOpts);
-		} catch(MqttException me) {
-			exeptionMessage(me);
-		}
-	}
-	public void disconnect() {
-		try {
-			sampleClient.disconnect();
-		} catch(MqttException me) {
+		} catch (MqttException me) {
 			exeptionMessage(me);
 		}
 	}
 
+	public void disconnect() {
+		try {
+			sampleClient.disconnect();
+		} catch (MqttException me) {
+			exeptionMessage(me);
+		}
+	}
 
 	public void subscribe() {
 		try {
 			sampleClient.subscribe(topic);
-		} catch(MqttException me) {
+		} catch (MqttException me) {
 			exeptionMessage(me);
-		} 
-	}
-	
-	public void exeptionMessage(MqttException me) {
-		System.out.println("reason "+me.getReasonCode());
-		System.out.println("msg "+me.getMessage());
-		System.out.println("loc "+me.getLocalizedMessage());
-		System.out.println("cause "+me.getCause());
-		System.out.println("excep "+me);
-		me.printStackTrace();
+		}
 	}
 
+	public void exeptionMessage(MqttException me) {
+		System.out.println("reason " + me.getReasonCode());
+		System.out.println("msg " + me.getMessage());
+		System.out.println("loc " + me.getLocalizedMessage());
+		System.out.println("cause " + me.getCause());
+		System.out.println("excep " + me);
+		me.printStackTrace();
+	}
 }
