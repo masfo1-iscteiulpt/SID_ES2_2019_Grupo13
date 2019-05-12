e47c90242958fbd9566d115079bc3341f4d7a8e4
reavh-iscteiul <reavh@iscte-iul.pt>
2019-05-12 16:11:25 +0100

    Created GUI projects

---
+++ GUI/Administrador/src/main/java/Sid_Grupo13/Administrador/App.java
@@ -0,0 +1,13 @@
+package Sid_Grupo13.Administrador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GUI/Investigador/src/main/java/Sid_Grupo13/Investigador/App.java
@@ -0,0 +1,13 @@
+package Sid_Grupo13.Investigador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
