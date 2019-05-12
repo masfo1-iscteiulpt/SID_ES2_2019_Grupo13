6d08194615b9445bc989461b9c96a3dd7a5b3ed0
reavh-iscteiul <reavh@iscte-iul.pt>
2019-05-12 19:39:44 +0100

    Fixed functionality

--- GUI/Investigador/src/main/java/Sid_Grupo13/Investigador/MainController.java
+++ GUI/Investigador/src/main/java/Sid_Grupo13/Investigador/MainController.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 
 public class MainController implements Initializable {
 
@@ -14,16 +16,27 @@ public class MainController implements Initializable {
 
 	public TableView<Cultura> culturasTable;
 	public TableView<Medicao> medicoesTable;
+	
+	public VBox culturasPane;
+	public VBox medicoesPane;
 
 	public void initialize(URL location, ResourceBundle resources) {
 
 	}
 
 	public void showCulturas() {
-
+		showCulturas.setStyle("-fx-background-color: #ffffff");
+		showCulturas.setTextFill(Paint.valueOf("#000000"));
+		showMedicoes.setStyle("-fx-background-color: #05BC78");
+		showMedicoes.setTextFill(Paint.valueOf("#ffffff"));
+		culturasPane.toFront();
 	}
 
 	public void showMedicoes() {
-
+		showMedicoes.setStyle("-fx-background-color: #ffffff");
+		showMedicoes.setTextFill(Paint.valueOf("#000000"));
+		showCulturas.setStyle("-fx-background-color: #05BC78");
+		showCulturas.setTextFill(Paint.valueOf("#ffffff"));
+		medicoesPane.toFront();
 	}
 }
