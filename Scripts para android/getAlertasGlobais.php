8812bd17529d4f42337ab50794dfa5f1b06f7c89
ddmso-iscteiul <ddmso@iscte-iul.pt>
2019-05-12 17:10:49 +0100

    finished adding php scripts and added sql script for db generation

---
+++ Scripts para android/getAlertasGlobais.php
@@ -0,0 +1,26 @@
+<?php
+
+	$username = $_GET['username'];
+	$password = $_GET['password'];
+	$date= $_GET['date'];
+	$url = 'localhost';
+	$conn = mysqli_connect($url, $username, $password);
+
+	if (!$conn) {
+		die("ConnectionFailled: " . $conn->connect_error);
+	}
+	
+	$sql = "call getAlertasGlobais(".$date.");";
+	$result = mysqli_query($conn, $sql);
+	$rows = array();
+	if ($result) {
+		if (mysqli_num_rows($result)>0){
+			while($r=mysqli_fetch_assoc($result)){
+				array_push($rows, $r);
+			}
+		}
+	}
+	mysqli_close ($conn);
+	return json_encode($rows);
+
+?>
