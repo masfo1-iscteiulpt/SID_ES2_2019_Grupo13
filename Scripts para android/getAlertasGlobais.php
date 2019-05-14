acf73ccce77c373fe5123a9182c50c3fd1454103
reavh-iscteiul <43678772+reavh-iscteiul@users.noreply.github.com>
2019-05-14 15:43:12 +0100

    Changed getAlertasGlobais.php

--- Scripts para android/getAlertasGlobais.php
+++ Scripts para android/getAlertasGlobais.php
@@ -1,28 +1,26 @@
 <?php
 
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-	$date= $_GET['date'];
-	$url = 'localhost';
-	$database = 'sid2019'
-	$conn = mysqli_connect($url, $username, $password,$database);
-
+	$url = "127.0.0.1";
+	$database = "sid2019";
+	$conn = mysqli_connect($url, $_POST['username'], $_POST['password'], $database);
 
 	if (!$conn) {
 		die("ConnectionFailled: " . $conn->connect_error);
 	}
-	
-	$sql = "call getAlertasGlobais(".$date.");";
+
+	$sql = "CALL getAlertasGlobais(".$_POST['date'].");";
 	$result = mysqli_query($conn, $sql);
 	$rows = array();
+
 	if ($result) {
-		if (mysqli_num_rows($result)>0){
-			while($r=mysqli_fetch_assoc($result)){
+		if (mysqli_num_rows($result) > 0) {
+			while ($r = mysqli_fetch_assoc($result)) {
 				array_push($rows, $r);
 			}
 		}
 	}
-	mysqli_close ($conn);
+
+	mysqli_close($conn);
 	return json_encode($rows);
 
 ?>
