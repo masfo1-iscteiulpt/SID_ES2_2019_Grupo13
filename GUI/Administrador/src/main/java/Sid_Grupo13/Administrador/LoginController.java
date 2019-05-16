0526537d9555f6cb6e41c3d87d8f9690bb6fd2e0
reavh-iscteiul <reavh@iscte-iul.pt>
2019-05-17 00:09:15 +0100

    Added IP address and Port fields

--- GUI/Administrador/src/main/java/Sid_Grupo13/Administrador/LoginController.java
+++ GUI/Administrador/src/main/java/Sid_Grupo13/Administrador/LoginController.java
@@ -1,13 +1,16 @@
 package Sid_Grupo13.Administrador;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,17 +19,24 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
 	public TextField username;
 	public PasswordField password;
+	public TextField address;
+	public TextField port;
 	public Label error;
+
+	public void initialize(URL location, ResourceBundle resources) {
+		address.setText("localhost");
+		port.setText("3306");
+	}
 
 	@FXML
 	private void login(ActionEvent actionEvent) throws IOException {
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/teste5?user="
-					+ username.getText() + "&password=" + password.getText());
+			Connection connection = DriverManager.getConnection("jdbc:mariadb://" + address.getText() + ":"
+					+ port.getText() + "/sid2019?user=" + username.getText() + "&password=" + password.getText());
 
 			((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
 			openMainWindow(connection);
